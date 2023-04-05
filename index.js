b90fcf5394e81a68a2c94c36944ba24f15c9c521
mukesgdbit13494 <mukesgdbit13494@gmail.com>
2023-04-05 08:09:06 +0530

    added console 1

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const app = express();
 
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, "public"))); //  "public" off of current is root
-
+console.log("hello this my branch code");
 const asyncLocalStorage = new AsyncLocalStorage();
 
 const requestIdMiddleware = (req, res, next) => {
